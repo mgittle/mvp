47206bfe56fd60a7cb0b1c40678c9f36f2adbafa
Matthew Gittleman <gittleman.matthew@gmail.com>
2020-02-26 14:01:59 -0600

    react navigation refactor

--- App.js
+++ App.js
@@ -1,72 +1,95 @@
-import React, { Component } from "react";
-import { StyleSheet, View, ImageBackground } from "react-native";
+// import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomepageOuter from "./components/HomepageOuter";
 import Homepage from "./components/Homepage";
-import img from "./images/MediumPile.jpg";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasError: false,
-      players: 2,
-      imageLoaded: false
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleValueChange = this.handleValueChange.bind(this);
-    this.handleImageLoaded = this.handleImageLoaded.bind(this);
-  }
+const Stack = createStackNavigator();
 
-  static getDerivedStateFromError(error) {
-    this.setState(error => ({ hasError: true }));
-  }
-
-  componentDidCatch(error, errorInfo) {
-    logErrorToMyService(error, errorInfo);
-  }
-
-  handleImageLoaded() {
-    this.setState(() => ({ imageLoaded: true }));
-  }
-
-  handleValueChange(itemValue, itemIndex) {
-    this.setState(() => ({ players: itemValue }));
-  }
-
-  handleSubmit() {
-    console.log(this.state.players);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h1>The frost is on the pumpkin.</h1>;
-    }
-    return (
-      <View style={styles.flexify}>
-        <ImageBackground
-          source={img}
-          style={styles.backgroundify}
-          imageStyle={{ opacity: 0.3 }}
-          onLoad={this.handleImageLoaded}
-        >
-          <Homepage
-            imageLoaded={this.state.imageLoaded}
-            players={this.state.players}
-            handleValueChange={this.handleValueChange}
-            handleSubmit={this.handleSubmit}
-          />
-        </ImageBackground>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomepageOuter}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-  flexify: {
-    flex: 1
-  },
-  backgroundify: {
-    flex: 1,
-    width: "100%",
-    height: "100%"
-  }
-});
+// import React, { Component } from "react";
+// import { StyleSheet, View, ImageBackground } from "react-native";
+// import Homepage from "./components/Homepage";
+// import img from "./images/MediumPile.jpg";
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       hasError: false,
+//       players: 2,
+//       imageLoaded: false
+//     };
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleValueChange = this.handleValueChange.bind(this);
+//     this.handleImageLoaded = this.handleImageLoaded.bind(this);
+//   }
+
+//   static getDerivedStateFromError(error) {
+//     this.setState(error => ({ hasError: true }));
+//   }
+
+//   componentDidCatch(error, errorInfo) {
+//     logErrorToMyService(error, errorInfo);
+//   }
+
+//   handleImageLoaded() {
+//     this.setState(() => ({ imageLoaded: true }));
+//   }
+
+//   handleValueChange(itemValue, itemIndex) {
+//     this.setState(() => ({ players: itemValue }));
+//   }
+
+//   handleSubmit() {
+//     console.log(this.state.players);
+//   }
+
+//   render() {
+//     if (this.state.hasError) {
+//       return <h1>The frost is on the pumpkin.</h1>;
+//     }
+//     return (
+//       <View style={styles.flexify}>
+//         <ImageBackground
+//           source={img}
+//           style={styles.backgroundify}
+//           imageStyle={{ opacity: 0.3 }}
+//           onLoad={this.handleImageLoaded}
+//         >
+//           <Homepage
+//             imageLoaded={this.state.imageLoaded}
+//             players={this.state.players}
+//             handleValueChange={this.handleValueChange}
+//             handleSubmit={this.handleSubmit}
+//           />
+//         </ImageBackground>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   flexify: {
+//     flex: 1
+//   },
+//   backgroundify: {
+//     flex: 1,
+//     width: "100%",
+//     height: "100%"
+//   }
+// });
---
+++ components/HomepageOuter.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { StyleSheet, View, ImageBackground } from "react-native";
+import Homepage from "./Homepage";
+import img from "../images/MediumPile.jpg";
+
+export default class HomepageOuter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      players: 2,
+      imageLoaded: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
+    this.handleImageLoaded = this.handleImageLoaded.bind(this);
+  }
+
+  static getDerivedStateFromError(error) {
+    this.setState(error => ({ hasError: true }));
+  }
+
+  componentDidCatch(error, errorInfo) {
+    logErrorToMyService(error, errorInfo);
+  }
+
+  handleImageLoaded() {
+    this.setState(() => ({ imageLoaded: true }));
+  }
+
+  handleValueChange(itemValue, itemIndex) {
+    this.setState(() => ({ players: itemValue }));
+  }
+
+  handleSubmit() {
+    console.log(this.state.players);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>The frost is on the pumpkin.</h1>;
+    }
+    return (
+      <View style={styles.flexify}>
+        <ImageBackground
+          source={img}
+          style={styles.backgroundify}
+          imageStyle={{ opacity: 0.3 }}
+          onLoad={this.handleImageLoaded}
+        >
+          <Homepage
+            imageLoaded={this.state.imageLoaded}
+            players={this.state.players}
+            handleValueChange={this.handleValueChange}
+            handleSubmit={this.handleSubmit}
+          />
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  flexify: {
+    flex: 1
+  },
+  backgroundify: {
+    flex: 1,
+    width: "100%",
+    height: "100%"
+  }
+});
